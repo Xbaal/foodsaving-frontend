d0b9f642914b23da31f7e85f76d1f04927983727
Tilmann Becker <tilmann.becker@gmail.com>
2016-10-08 00:57:49 +0200

    WIP: rewrite login directive

--- client/app/components/login/login.controller.js
+++ client/app/components/login/login.controller.js
@@ -1,10 +1,25 @@
 class LoginController {
-  constructor($scope) {
+  constructor($scope, $state, Authentication) {
     "ngInject";
     this.name = "login";
+    this.$scope = $scope;
+    this.$state = $state;
+    this.Authentication = Authentication;
     $scope.user = "";
     $scope.password = "";
     $scope.remember = false;
+  }
+
+  login() {
+    this.Authentication.login(this.$scope.user, this.$scope.password)
+    .then(() => {
+      this.$scope.loginStatus = "success";
+      this.$scope.loginForm.$error.failed = false;
+      this.$state.go("home");
+    }).catch(() => {
+      this.$scope.loginForm.$error.failed = true;
+      this.$scope.password = "";
+    });
   }
 }
 
